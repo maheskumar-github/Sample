2ad84642fd28177a2cb11a8b990151fd9f845069
Maheskumar P <maheskumar.palani@gmail.com>
2020-05-21 22:06:04 +0530

    Change the local file

---
+++ DEMOPROJECT/DEMOAPP/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def hi(request):
+    #return HttpResponse('<h1>Welcome Maheskumar Palanimuthu!!!</h1>')
+    return render(request, 'DEMOAPP/index.html')
